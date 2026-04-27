6433a443011795af4757b047d4fa6f0db9ae8160
anandudileep <Anandu@owleto.in>
2026-04-27 16:26:19 +0530

    Deployment changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,6 +18,7 @@ const config = defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts: ['dev.open-wearable.cureo.city'],
     watch: {
       usePolling: true,
     },
